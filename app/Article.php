8181a0ba85dde2be6593a0bf57fdff2b48793ad3
hoge huga <lagoon1981@hotmail.com>
2016-02-04 12:15:27 +0900

    part 33 までおわり
    多対多のリレーションについて学んだ

--- app/Article.php
+++ app/Article.php
@@ -20,4 +20,9 @@ class Article extends Model
     {
         return $this->belongsTo('App\User');
     }
+
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag')->withTimestamps();
+    }
 }
