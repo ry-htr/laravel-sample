6fb9d4a3da982400c6245e804a349ac0e07cfd13
hoge huga <lagoon1981@hotmail.com>
2016-02-04 16:18:05 +0900

    サービスコンテナのバインド

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -45,3 +45,75 @@ Route::group(['middleware' => 'web'], function () {
 
     Route::resource('articles', 'ArticlesController');
 });
+
+// DIコンテナの使い方
+interface SenderInterface {
+    public function send($message);
+}
+
+class MailSender implements SenderInterface {
+    public function send($message) {
+        // ここで、メールでメッセージを送る
+
+        return "メールで $message を送りました。";
+    }
+}
+
+class BikeSender implements SenderInterface {
+    public function send($message) {
+        // ここで、バイクに乗ってメッセージを届ける
+
+        return "バイク便で $message を届けました。";
+    }
+}
+
+/*
+ *  Binding
+ * @var Illiminate\Foundation\Application $this->app
+ */
+$this->app->bind('sender', 'MailSender');
+$this->app->singleton('sender_single', 'MailSender');
+
+$instance = new MailSender();
+$this->app->instance('sender_instance', $instance);
+
+/*
+ * Routes
+ */
+Route::get('send/{message?}', function($message = '合格通知') {
+    $sender = $this->app->make('sender');
+
+    return $sender->send($message);
+});
+
+Route::get('not_singleton', function() {
+    $sender1 = $this->app->make('sender');
+    $sender2 = $this->app->make('sender');
+
+    if ($sender1 === $sender2) {
+        return "シングルトンです。";
+    }
+
+    return "シングルトンではありません。";
+});
+
+Route::get('singleton', function() {
+    $sender1 = $this->app->make('sender_single');
+    $sender2 = $this->app->make('sender_single');
+
+    if ($sender1 === $sender2) {
+        return "シングルトンです。";
+    }
+
+    return "シングルトンではありません。";
+});
+
+Route::get('instance', function() use ($instance) {   // ② 追加
+    $sender = $this->app->make('sender_instance');    // ③
+
+    if ($instance === $sender) {
+        return "既存のインスタンスです。";
+    }
+
+    return "既存のインスタンスではありません。";
+});
