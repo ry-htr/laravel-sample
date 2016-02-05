07da4176da10ef4880fbccd9d8925e1d5c90c525
hoge huga <lagoon1981@hotmail.com>
2016-02-05 10:54:36 +0900

    クーロン処理の書き方

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,6 +20,11 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
+     * set crontab
+     * ```
+     *   * * * * * php /path/to/artisan schedule:run
+     * ```
+     *
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
@@ -27,5 +32,25 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('inspire')
                  ->hourly();
+
+        /*
+        $schedule
+            ->command('auth:clear-reminders') // artisan command を実行
+            ->daily();  // 毎日
+
+        $schedule
+            ->call('YourClass@someMethod') // クラスメソッドを実行
+            ->hourly();  // 毎時
+
+        $schedule
+            ->call(function() {  // クロージャーを実行
+                // Do some task...
+            })
+            ->dailyAt('15:00');  // 毎日時間指定
+
+        $schedule
+            ->exec('cp oldThing newThing')   // ターミナルコマンドを実行
+            ->weekly()->mondays()->at('13:00');
+        */
     }
 }
